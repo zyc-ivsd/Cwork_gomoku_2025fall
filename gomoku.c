9583cb90022391995bda26508c9ce5fce5a1cfb2
zyc-ivsd <736215675@qq.com>
2025-12-07 22:56:31 +0800

    seven

--- gomoku.c
+++ gomoku.c
@@ -52,6 +52,11 @@ typedef struct {
 // 初始化游戏状态的函数
 void initializeGame(GameState *gameState) {
     gameState->currentPlayer = PLAYER_BLACK;
+    gameState->lastMove.col = -1;    
+    gameState->lastMove.row = -1;
+//刚开始执行程序的的时候，有莫名其妙的输出：Your last move was A2
+//经询问AI，得知是因为没有初始化lastmove，分配的是垃圾值，所以这里给个无效初始化
+
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
             gameState->board[i][j] = EMPTY;
@@ -65,7 +70,10 @@ void initializeGame(GameState *gameState) {
 // 打印棋盘的函数。使用数字表示列，字符表示行
 void printBoard(GameState *gameState) {
     // Display the last move
-    printf("Your last move was: %c%d\n", (char)gameState->lastMove.col + 'A', gameState->lastMove.row + 1);
+    if(gameState->lastMove.col < 0 || gameState->lastMove.row < 0){
+        printf("No move now\n");
+    }
+    else printf("Your last move was: %c%d\n", (char)gameState->lastMove.col + 'A', gameState->lastMove.row + 1);
 
     for (int i = 0; i < BOARD_SIZE; i++) {
         printf("%2d ", BOARD_SIZE - i);
